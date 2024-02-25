40de86991f99bd422b4d1a08bce82ba13c5fb6cb
AlectoWiPPle <gavlas07@gmail.com>
2024-02-25 14:56:35 +0200

    Main mechanic + Start menu

---
+++ 3d_Platformer/Assets/MovementScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float movementSpeed = 6f;
+    [SerializeField] float jumpForce = 5f;
+
+    int JumpCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        
+        rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
+        if (Input.GetButtonDown("Jump")&& JumpCount<10)
+        {
+            Jump();
+            JumpCount++;
+        }
+        
+    }
+    
+    void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+    }
+    
+
+
+}
